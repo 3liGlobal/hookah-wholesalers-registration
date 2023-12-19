39c6928b23b831adf231c22d8662e3e9a5a9374d
UmerK129 <120571513+UmerK129@users.noreply.github.com>
2023-12-19 17:43:39 +0500

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,14 +1,102 @@
 // $Id: $
 function zf_ValidateAndSubmit() {
-  if (zf_CheckMandatory()) {
-    if (zf_ValidCheck()) {
+  if (zf_CheckMandatory() && zf_ValidCheck()) {
+    $(document).ready(function () {
+      console.log("docum");
+      var current_fs, next_fs, previous_fs; //fieldsets
+      var opacity;
+      var current = 1;
+      var steps = $("fieldset").length;
+      // debugger;
+      setProgressBar(current);
+      // $(".next").click(function () {
+
+      current_fs = $(".next").parent();
+      next_fs = $(".next").parent().next();
+
+      //Add Class Active
+      $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+      $("#progressbar li")
+        .eq($("fieldset").index(current_fs))
+        .removeClass("active");
+
+      //show the next fieldset
+      next_fs.show();
+      //hide the current fieldset with style
+      current_fs.animate(
+        { opacity: 0 },
+        {
+          step: function (now) {
+            // for making fielset appear animation
+            opacity = 1 - now;
+
+            current_fs.css({
+              display: "none",
+              position: "relative",
+            });
+            next_fs.css({ opacity: opacity });
+          },
+          duration: 500,
+        }
+      );
+      setProgressBar(++current);
+      // });
+
+      $(".previous").click(function () {
+        current_fs = $(this).parent();
+        previous_fs = $(this).parent().prev();
+
+        //Remove class active
+        $("#progressbar li")
+          .eq($("fieldset").index(current_fs))
+          .removeClass("active");
+        $("#progressbar li")
+          .eq($("fieldset").index(previous_fs))
+          .addClass("active");
+
+        //show the previous fieldset
+        previous_fs.show();
+
+        //hide the current fieldset with style
+        current_fs.animate(
+          { opacity: 0 },
+          {
+            step: function (now) {
+              // for making fielset appear animation
+              opacity = 1 - now;
+
+              current_fs.css({
+                display: "none",
+                position: "relative",
+              });
+              previous_fs.css({ opacity: opacity });
+            },
+            duration: 500,
+          }
+        );
+        setProgressBar(--current);
+      });
+
+      function setProgressBar(curStep) {
+        var percent = parseFloat(100 / steps) * curStep;
+        percent = percent.toFixed();
+        $(".progress-bar").css("width", percent + "%");
+      }
       if (isSalesIQIntegrationEnabled) {
         zf_addDataToSalesIQ();
       }
       return true;
-    } else {
-      return false;
-    }
+      // $(".submit").click(function () {
+      //   return false;
+      // });
+
+      // if () {
+
+      //   return true;
+      // } else {
+      //   return false;
+      // }
+    });
   } else {
     return false;
   }
